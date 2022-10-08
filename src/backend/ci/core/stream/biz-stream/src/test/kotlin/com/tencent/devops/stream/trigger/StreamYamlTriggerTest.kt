2a1c15ba3776308a1a8690cb700a0c6ea62ace5e
ruotiantang <ruotiantang@tencent.com>
2022-10-08 15:01:42 +0800

    feat: add unit test demo

--- src/backend/ci/core/stream/biz-stream/src/test/kotlin/com/tencent/devops/stream/trigger/StreamYamlTriggerTest.kt
+++ src/backend/ci/core/stream/biz-stream/src/test/kotlin/com/tencent/devops/stream/trigger/StreamYamlTriggerTest.kt
@@ -29,66 +29,55 @@ package com.tencent.devops.stream.trigger
 
 import com.tencent.devops.common.api.enums.ScmType
 import com.tencent.devops.common.client.Client
-import com.tencent.devops.common.pipeline.enums.StartType
 import com.tencent.devops.common.webhook.pojo.code.git.GitCommit
 import com.tencent.devops.common.webhook.pojo.code.git.GitCommitAuthor
-import com.tencent.devops.common.webhook.pojo.code.git.GitCommitRepository
 import com.tencent.devops.common.webhook.pojo.code.git.GitMRAttributes
 import com.tencent.devops.common.webhook.pojo.code.git.GitMergeRequestEvent
 import com.tencent.devops.common.webhook.pojo.code.git.GitProject
-import com.tencent.devops.common.webhook.pojo.code.git.GitPushEvent
 import com.tencent.devops.common.webhook.pojo.code.git.GitUser
 import com.tencent.devops.process.api.service.ServicePipelineSettingResource
+import com.tencent.devops.process.yaml.v2.enums.TemplateType
+import com.tencent.devops.process.yaml.v2.exception.YamlFormatException
 import com.tencent.devops.process.yaml.v2.models.PreScriptBuildYaml
-import com.tencent.devops.process.yaml.v2.models.RepositoryHook
+import com.tencent.devops.process.yaml.v2.models.PreTemplateScriptBuildYaml
+import com.tencent.devops.process.yaml.v2.models.Resources
+import com.tencent.devops.process.yaml.v2.models.ResourcesPools
 import com.tencent.devops.process.yaml.v2.models.ScriptBuildYaml
-import com.tencent.devops.process.yaml.v2.models.Variable
+import com.tencent.devops.process.yaml.v2.models.TemplateData
+import com.tencent.devops.process.yaml.v2.models.TransferTemplateData
+import com.tencent.devops.process.yaml.v2.models.YamlTransferData
 import com.tencent.devops.process.yaml.v2.models.YmlName
-import com.tencent.devops.process.yaml.v2.models.on.TriggerOn
 import com.tencent.devops.process.yaml.v2.utils.ScriptYmlUtils
 import com.tencent.devops.project.api.service.ServiceProjectResource
 import com.tencent.devops.project.pojo.Result
 import com.tencent.devops.stream.config.StreamGitConfig
-import com.tencent.devops.stream.dao.GitPipelineResourceDao
 import com.tencent.devops.stream.dao.GitRequestEventBuildDao
-import com.tencent.devops.stream.dao.StreamBasicSettingDao
-import com.tencent.devops.stream.pojo.GitRequestEvent
 import com.tencent.devops.stream.pojo.TriggerReviewSetting
+import com.tencent.devops.stream.pojo.enums.TriggerReason
 import com.tencent.devops.stream.service.StreamBasicSettingService
-import com.tencent.devops.stream.service.StreamPipelineBranchService
-import com.tencent.devops.stream.trigger.actions.BaseAction
 import com.tencent.devops.stream.trigger.actions.data.ActionData
-import com.tencent.devops.stream.trigger.actions.data.ActionMetaData
 import com.tencent.devops.stream.trigger.actions.data.EventCommonData
 import com.tencent.devops.stream.trigger.actions.data.EventCommonDataCommit
 import com.tencent.devops.stream.trigger.actions.data.StreamTriggerPipeline
 import com.tencent.devops.stream.trigger.actions.data.StreamTriggerSetting
 import com.tencent.devops.stream.trigger.actions.data.context.StreamTriggerContext
-import com.tencent.devops.stream.trigger.actions.streamActions.StreamMrAction
 import com.tencent.devops.stream.trigger.actions.tgit.TGitMrActionGit
-import com.tencent.devops.stream.trigger.actions.tgit.TGitPushActionGit
+import com.tencent.devops.stream.trigger.exception.StreamTriggerBaseException
 import com.tencent.devops.stream.trigger.exception.StreamTriggerException
-import com.tencent.devops.stream.trigger.git.pojo.StreamGitCred
-import com.tencent.devops.stream.trigger.git.pojo.StreamGitProjectInfo
+import com.tencent.devops.stream.trigger.exception.YamlBehindException
+import com.tencent.devops.stream.trigger.exception.YamlBlankException
 import com.tencent.devops.stream.trigger.git.pojo.tgit.TGitProjectInfo
-import com.tencent.devops.stream.trigger.git.service.StreamGitApiService
 import com.tencent.devops.stream.trigger.git.service.TGitApiService
 import com.tencent.devops.stream.trigger.parsers.PipelineDelete
 import com.tencent.devops.stream.trigger.parsers.triggerMatch.TriggerBody
 import com.tencent.devops.stream.trigger.parsers.triggerMatch.TriggerMatcher
 import com.tencent.devops.stream.trigger.parsers.triggerMatch.TriggerResult
+import com.tencent.devops.stream.trigger.parsers.yamlCheck.YamlFormat
 import com.tencent.devops.stream.trigger.parsers.yamlCheck.YamlSchemaCheck
-import com.tencent.devops.stream.trigger.pojo.MrCommentBody
 import com.tencent.devops.stream.trigger.pojo.MrYamlInfo
-import com.tencent.devops.stream.trigger.pojo.YamlContent
-import com.tencent.devops.stream.trigger.pojo.YamlPathListEntry
 import com.tencent.devops.stream.trigger.pojo.YamlReplaceResult
-import com.tencent.devops.stream.trigger.pojo.enums.StreamCommitCheckState
-import com.tencent.devops.stream.trigger.service.DeleteEventService
 import com.tencent.devops.stream.trigger.service.GitCheckService
-import com.tencent.devops.stream.trigger.service.StreamEventService
 import com.tencent.devops.stream.trigger.template.YamlTemplateService
-import com.tencent.devops.stream.trigger.timer.service.StreamTimerService
 import io.mockk.every
 import io.mockk.justRun
 import io.mockk.mockk
@@ -97,13 +86,12 @@ import io.mockk.spyk
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.mock
+import org.mockito.kotlin.any
 
 @DisplayName("Stream Yaml触发过程测试")
 internal class StreamYamlTriggerTest {
@@ -118,10 +106,9 @@ internal class StreamYamlTriggerTest {
     private val gitRequestEventBuildDao = mockk<GitRequestEventBuildDao>()
     private val streamYamlBaseBuild = mockk<StreamYamlBaseBuild>()
 
-    // TODO: 这里有个踩出的小坑，对于无参函数mockk的模拟存在问题导致无法触发，所以使用mockito
-    // 同时mockito对于kotlin fun a = xxx 的写法不支持，所以也要修改函数
-    private val streamGitConfig = mock<StreamGitConfig> {
-        on { getScmType() } doReturn ScmType.CODE_GIT
+    // TODO: 存在IDEA进行单元测试时报错，重试后可以恢复
+    private val streamGitConfig = mockk<StreamGitConfig> {
+        every { getScmType() } returns ScmType.CODE_GIT
     }
 
     private val trigger = spyk(
@@ -216,15 +203,17 @@ internal class StreamYamlTriggerTest {
             )
         }
         action = spyk(
-            spyk(TGitMrActionGit(
-                dslContext = dslContext,
-                streamSettingDao = mockk(),
-                apiService = apiService,
-                mrConflictCheck = mockk(),
-                pipelineDelete = pipelineDelete,
-                gitCheckService = gitCheckService,
-                streamTriggerTokenService = mockk()
-            ).also { it.data = actionData })
+            spyk(
+                TGitMrActionGit(
+                    dslContext = dslContext,
+                    streamSettingDao = mockk(),
+                    apiService = apiService,
+                    mrConflictCheck = mockk(),
+                    pipelineDelete = pipelineDelete,
+                    gitCheckService = gitCheckService,
+                    streamTriggerTokenService = mockk()
+                ).also { it.data = actionData }
+            )
         )
     }
 
@@ -237,7 +226,8 @@ internal class StreamYamlTriggerTest {
         fun test_1() {
             val triggerStr = "trigger"
             every { triggerMatcher.isMatch(action, triggerStr) } returns Pair(
-                null, TriggerResult(
+                null,
+                TriggerResult(
                     trigger = TriggerBody(trigger = false),
                     startParams = emptyMap(),
                     timeTrigger = true,
@@ -326,8 +316,10 @@ internal class StreamYamlTriggerTest {
 
             Assertions.assertThrows(StreamTriggerException::class.java) {
                 trigger.triggerBuild(
-                    action, Pair(
-                        null, TriggerResult(
+                    action,
+                    Pair(
+                        null,
+                        TriggerResult(
                             trigger = TriggerBody(false),
                             startParams = emptyMap(),
                             timeTrigger = false,
@@ -338,9 +330,14 @@ internal class StreamYamlTriggerTest {
             }
         }
 
-        @Test
-        @DisplayName("流水线ID为空 | 分支测试")
-        fun test_2() {
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "流水线ID为空 | 分支测试",
+                "流水线ID为空，yamlObject.name.isBlank | 分支测试",
+            ]
+        )
+        fun test_2(param: String) {
             actionData.context.pipeline = actionData.context.pipeline?.copy(
                 gitProjectId = "123"
             )
@@ -392,7 +389,11 @@ internal class StreamYamlTriggerTest {
                 ),
                 ScriptBuildYaml(
                     version = null,
-                    name = null,
+                    name = if (param == "流水线ID为空，yamlObject.name.isBlank | 分支测试") {
+                        ""
+                    } else {
+                        null
+                    },
                     label = null,
                     triggerOn = null,
                     variables = null,
@@ -434,5 +435,156 @@ internal class StreamYamlTriggerTest {
 
             Assertions.assertTrue(trigger.triggerBuild(action, null))
         }
+    }
+
+    @Nested
+    @DisplayName("prepareCIBuildYaml 测试组")
+    inner class PrepareCIBuildYamlTest {
+
+        @DisplayName("originYaml 为空 | 分支测试")
+        @Test
+        fun test_1() {
+            actionData.context.originYaml = ""
+            Assertions.assertEquals(null, trigger.prepareCIBuildYaml(action))
+        }
+
+        @DisplayName("整体覆盖测试")
+        @Test
+        fun test_2() {
+            mockkObject(YamlFormat)
+            mockkObject(ScriptYmlUtils)
+
+            action.data.context.originYaml = "version: v2.0"
+
+            every { YamlFormat.formatYaml(action) } returns PreTemplateScriptBuildYaml(
+                version = null,
+                name = null,
+                label = null,
+                triggerOn = null,
+                variables = null,
+                stages = null,
+                jobs = null,
+                steps = null,
+                extends = null,
+                resources = Resources(
+                    null, pools = listOf(ResourcesPools("", ""))
+                ),
+                notices = null,
+                finally = null,
+                concurrency = null
+            )
+
+            // 测试1：正常返回，分支覆盖
+            val expectData = YamlTransferData(
+                TemplateData(
+                    transferDataMap = mutableMapOf(
+                        "" to TransferTemplateData("", TemplateType.VARIABLE, "")
+                    )
+                )
+            )
+
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } returns Pair(
+                ScriptBuildYaml(
+                    version = null,
+                    name = null,
+                    label = null,
+                    triggerOn = null,
+                    variables = null,
+                    stages = emptyList(),
+                    extends = null,
+                    resource = null,
+                    notices = null,
+                    finally = null,
+                    concurrency = null
+                ),
+                expectData
+            )
+
+            Assertions.assertTrue(trigger.prepareCIBuildYaml(action)!!.yamlTransferData == expectData)
+
+            // 测试2：正常返回，transferData.templateData.transferDataMap.isEmpty()，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } returns Pair(
+                ScriptBuildYaml(
+                    version = null,
+                    name = null,
+                    label = null,
+                    triggerOn = null,
+                    variables = null,
+                    stages = emptyList(),
+                    extends = null,
+                    resource = null,
+                    notices = null,
+                    finally = null,
+                    concurrency = null
+                ),
+                YamlTransferData()
+            )
+
+            Assertions.assertTrue(trigger.prepareCIBuildYaml(action)!!.yamlTransferData == null)
+
+            // 测试3：抛出 YamlBlankException 异常，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } throws YamlBlankException("", "")
+
+            Assertions.assertEquals(
+                TriggerReason.CI_YAML_CONTENT_NULL,
+                Assertions.assertThrows(StreamTriggerException::class.java) {
+                    trigger.prepareCIBuildYaml(action)
+                }.triggerReason
+            )
+
+            // 测试4：抛出 YamlBehindException 异常，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } throws YamlBehindException("")
+
+            Assertions.assertEquals(
+                TriggerReason.CI_YAML_NEED_MERGE_OR_REBASE,
+                Assertions.assertThrows(StreamTriggerException::class.java) {
+                    trigger.prepareCIBuildYaml(action)
+                }.triggerReason
+            )
+
+            // 测试5：抛出 YamlFormatException 异常，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } throws YamlFormatException("")
+
+            Assertions.assertEquals(
+                TriggerReason.CI_YAML_TEMPLATE_ERROR,
+                Assertions.assertThrows(StreamTriggerException::class.java) {
+                    trigger.prepareCIBuildYaml(action)
+                }.triggerReason
+            )
+
+            // 测试6：抛出 StackOverflowError 异常，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } throws StackOverflowError()
+
+            Assertions.assertEquals(
+                TriggerReason.CI_YAML_TEMPLATE_ERROR,
+                Assertions.assertThrows(StreamTriggerException::class.java) {
+                    trigger.prepareCIBuildYaml(action)
+                }.triggerReason
+            )
+
+            // 测试7：抛出 StreamTriggerBaseException 异常，分支覆盖
+            every {
+                ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any())
+            } throws StreamTriggerBaseException(action, null, null)
+
+            Assertions.assertThrows(StreamTriggerBaseException::class.java) { trigger.prepareCIBuildYaml(action) }
+
+            // 测试8：抛出 未知异常，分支覆盖
+            every { ScriptYmlUtils.normalizeGitCiYaml(any(), any(), any()) } throws RuntimeException()
+
+            Assertions.assertEquals(
+                TriggerReason.UNKNOWN_ERROR,
+                Assertions.assertThrows(StreamTriggerException::class.java) {
+                    trigger.prepareCIBuildYaml(action)
+                }.triggerReason
+            )
+        }
+    }
+
+    @DisplayName("补齐行覆盖率")
+    @Test
+    fun demoTest() {
+        Assertions.assertEquals("v2.0", StreamYamlTrigger.ymlVersion)
+        Assertions.assertEquals("STREAM_TEMPLATE_ROOT_FILE", StreamYamlTrigger.STREAM_TEMPLATE_ROOT_FILE)
     }
 }
